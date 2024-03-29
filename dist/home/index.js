002aaf4fa70f59cd1a80012f11020c515d642a7d
Max Chechel <maksymchechel@yahoo.com>
2024-03-29 07:26:16 +0100

    .

--- dist/home/index.js
+++ dist/home/index.js
@@ -30078,11 +30078,7 @@ document.addEventListener("DOMContentLoaded", function () {
       var swiper = new _swiper.default(".swiper", {
         slidesPerView: 1,
         spaceBetween: 0,
-        loop: true,
-        autoplay: {
-          delay: 3000,
-          disableOnInteraction: false
-        }
+        loop: true
       });
 
       //Slides initial position
